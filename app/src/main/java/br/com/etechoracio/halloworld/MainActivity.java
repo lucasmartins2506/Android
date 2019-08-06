5f3567a9188ad3fabc3dbebcd6552e1b16f5709f
Lucas <lucasnalua25@gmail.com>
2019-08-05 22:33:46 -0300

    Hello World - Android

---
+++ app/src/main/java/br/com/etechoracio/halloworld/MainActivity.java
@@ -0,0 +1,29 @@
+package br.com.etechoracio.halloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onOK(View view){
+
+
+        Toast msg = Toast.makeText(this,"HelloWorld",Toast.LENGTH_LONG);
+        msg.show();
+    }
+
+
+
+
+
+
+
+}
